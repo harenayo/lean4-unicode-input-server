c18b6a0d6e15b854bb1e3950967078d09ba86e35
Harenayo <123649644+harenayo@users.noreply.github.com>
2024-11-21 18:38:10 +0900

    Implement a simple server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use {
+    lsp_server::{
+        Connection,
+        Message,
+        ProtocolError,
+    },
+    serde_json::json,
+    std::error::Error,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let (connection, threads) = Connection::stdio();
+    run(connection)?;
+    threads.join()?;
+    Result::Ok(())
+}
+
+fn run(connection: Connection) -> Result<(), ProtocolError> {
+    connection.initialize(json!({
+        "capabilities": {},
+        "serverInfo": {
+            "name": env!("CARGO_PKG_NAME"),
+            "version": env!("CARGO_PKG_VERSION"),
+        },
+    }))?;
+
+    for message in &connection.receiver {
+        if let Message::Request(request) = message {
+            if connection.handle_shutdown(&request)? {
+                break;
+            }
+        }
+    }
+
+    Result::Ok(())
 }
